c1fc031dfb5a774ccc4b9d37657e30db1e2a0721
Khai Fahmi Zaki <khaifahmi99@gmail.com>
2024-06-15 01:32:06 +1000

    fix: add button in hero to section link

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,6 +4,7 @@ import Travel1 from '../../public/travel1.jpeg';
 import Travel2 from '../../public/travel2.jpeg';
 import Food1 from '../../public/food1.jpeg';
 import { FaArrowRight } from "react-icons/fa6";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -18,12 +19,19 @@ export default function Hero() {
               <div className="py-8 lg:py-12">
                 <h2>Welcome to a journey through my experiences!<br /><br />This gallery is a glimpse into my travels, the delicious food I&lsquo;ve encountered, and the unique collections I&lsquo;ve built along the way. Each image, story, and treasure tells a tale of exploration, discovery, and the joy of finding beauty in the unexpected. Dive in and explore the world through my lens, where every bite, sight, and find becomes a cherished memory.</h2>
               </div>
-              {/* TODO: Handle onClick */}
-              <div className="hover:bg-emerald-100 w-4/5 md:w-1/3 cursor-pointer flex flex-row justify-between text-zinc-900 px-4 py-2 bg-white rounded border-emerald-400 border-2 items-center">
-                <div className='text-lg'>
-                  Explore More
-                </div>
-                <div className="w-4 h-4"><FaArrowRight /></div>
+              <div className='flex flex-col md:flex-row gap-4'>
+                <Link href='/#travels' className="hover:bg-emerald-100 w-4/5 md:w-1/3 flex flex-row justify-between text-zinc-900 px-4 py-2 bg-white rounded border-emerald-400 border-2 items-center">
+                  <div className='text-lg'>
+                    Explore Travels
+                  </div>
+                  <div className="w-4 h-4"><FaArrowRight /></div>
+                </Link>
+                <Link href='/#eats' className="hover:bg-emerald-100 w-4/5 md:w-1/3 flex flex-row justify-between text-zinc-900 px-4 py-2 bg-white rounded border-emerald-400 border-2 items-center">
+                  <div className='text-lg'>
+                    Explore Eats
+                  </div>
+                  <div className="w-4 h-4"><FaArrowRight /></div>
+                </Link>
               </div>
             </div>
             <div className="hidden xl:block xl:col-span-5 justify-self-center relative">
--- app/page.tsx
+++ app/page.tsx
@@ -20,27 +20,31 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
       <Hero />
-      <SummaryWithTwoExamples 
-        title='TRAVELS' 
-        subtitle='One pushpin at a time across the globe' 
-        description="My mind won't be able to accurately remember places in detail over time, so I captured the memories while the view still stands. Hereby, I elect you to witness the beauty of the world through the lenses of my eyes. This is where dreams meet reality."
-        link='/travels'
-        contents={[
-          { src: Travel1, title: 'Athens, Greece', subtitle: 'Athens is a place where history whispers from every cobblestone.  Towering above the city, the Acropolis beckons you to walk in the footsteps of philosophers and gods.  But Athens is more than just ancient wonders.  Delve into bustling markets overflowing with fresh souvlaki and glistening olives, then lose yourself in the infectious energy of a taverna, clinking glasses with new friends under the warm Greek sun.' },
-          { src: Travel2, title: 'Rottnest Island, Australia', subtitle: 'Turquoise waters lap at sugar-white beaches on Rottnest Island, a car-free paradise just off the coast of Perth.  Here, cuddle-buddies reign supreme: the adorable quokka, a marsupial with an ever-present grin, awaits your encounter.  Beyond the wildlife, explore shipwrecks teeming with marine life or soak up the sun on a secluded cove – Rottnest Island is your slice of serenity Down Under.' }
-        ]}
-        theme='amber'
-      />
-      <SummaryWithGallery 
-        title='EATS' 
-        subtitle="Looks can be deceiving but these pictures won't!" 
-        description="The exhibition of my entire gallery of food I've tasted in the places where I had travelled to; embracing parallelism through visual connection. Meals, cuisines, desserts, and delectables, served on a plate as a feast to the audience."
-        link='/eats'
-        theme='rose'
-        images={[
-          Food1, Food2, Food3, Food4, Food5, Food6, Food7, Food8,
-        ]}
-      />
+      <div id='travels'>
+        <SummaryWithTwoExamples 
+          title='TRAVELS' 
+          subtitle='One pushpin at a time across the globe' 
+          description="My mind won't be able to accurately remember places in detail over time, so I captured the memories while the view still stands. Hereby, I elect you to witness the beauty of the world through the lenses of my eyes. This is where dreams meet reality."
+          link='/travels'
+          contents={[
+            { src: Travel1, title: 'Athens, Greece', subtitle: 'Athens is a place where history whispers from every cobblestone.  Towering above the city, the Acropolis beckons you to walk in the footsteps of philosophers and gods.  But Athens is more than just ancient wonders.  Delve into bustling markets overflowing with fresh souvlaki and glistening olives, then lose yourself in the infectious energy of a taverna, clinking glasses with new friends under the warm Greek sun.' },
+            { src: Travel2, title: 'Rottnest Island, Australia', subtitle: 'Turquoise waters lap at sugar-white beaches on Rottnest Island, a car-free paradise just off the coast of Perth.  Here, cuddle-buddies reign supreme: the adorable quokka, a marsupial with an ever-present grin, awaits your encounter.  Beyond the wildlife, explore shipwrecks teeming with marine life or soak up the sun on a secluded cove – Rottnest Island is your slice of serenity Down Under.' }
+          ]}
+          theme='amber'
+        />
+      </div>
+      <div id="eats">
+        <SummaryWithGallery 
+          title='EATS' 
+          subtitle="Looks can be deceiving but these pictures won't!" 
+          description="The exhibition of my entire gallery of food I've tasted in the places where I had travelled to; embracing parallelism through visual connection. Meals, cuisines, desserts, and delectables, served on a plate as a feast to the audience."
+          link='/eats'
+          theme='rose'
+          images={[
+            Food1, Food2, Food3, Food4, Food5, Food6, Food7, Food8,
+          ]}
+        />
+      </div>
       {/* TODO: Activate collections when section is visible */}
       {/* <SummaryWithExamples 
         title='COLLECTIONS' 
