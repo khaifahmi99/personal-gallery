eb0b502197d888a44ad3f149d606fd6f6835dd79
Khai Fahmi Zaki <khaifahmi99@gmail.com>
2024-06-15 13:47:42 +1000

    feat: make the counter animated

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { animate, motion, useInView, useMotionValue, useTransform } from "framer-motion";
+import { useEffect, useRef } from "react";
+
 export type CounterProps = {
   items: {
     name: string;
@@ -6,7 +11,7 @@ export type CounterProps = {
 }
 
 function formatNumberLocale(num: number) {
-  return num.toLocaleString('en-US');
+  return num.toLocaleString('en-US', { minimumFractionDigits: 0, maximumFractionDigits: 0 });
 }
 
 export default function Counter({ items }: CounterProps) {
@@ -15,11 +20,32 @@ export default function Counter({ items }: CounterProps) {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {items.map((item, index) => (
             <div key={index} className="flex flex-col items-center justify-center bg-transparent">
-              <h2 className="text-5xl font-bold">{formatNumberLocale(item.count)}</h2>
+              <AnimatedCounter to={item.count} />
               <p className="text-zinc-400">{item.name}</p>
             </div>
           ))}
         </div>
       </div>
   )
+}
+
+function AnimatedCounter({ to }: { to: number }) {
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => {
+    return formatNumberLocale(latest);
+  });
+
+  const ref = useRef(null);
+  const inView = useInView(ref);
+
+  // while in view animate the count
+  useEffect(() => {
+    if (inView) {
+      animate(count, to, { duration: 2 });
+
+
+    }
+  }, [count, inView, to]);
+
+  return <motion.span ref={ref} className="text-5xl font-bold">{rounded}</motion.span>;
 }
