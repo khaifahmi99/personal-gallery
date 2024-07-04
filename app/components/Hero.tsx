2fad901302e4d47a23f133ac387719f50f5c58bd
Khai Fahmi Zaki <khaifahmi99@gmail.com>
2024-07-04 23:15:00 +1000

    feat: add box reveal to hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,6 +7,7 @@ import { FaArrowRight } from "react-icons/fa6";
 import Link from "next/link";
 import Counter, { CounterProps } from "./Counter";
 import ShineBorder from "@/components/magicui/shine-border";
+import BoxReveal from "@/components/magicui/box-reveal";
 
 type Props = CounterProps;
 
@@ -15,29 +16,41 @@ export default function Hero(props: Props) {
     <div className='bg-grid'>
       <section className='w-full pt-48 pb-20 bg-gray-800 bg-radial'>
         <div className='container px-4 mx-auto flex text-zinc-100 flex-col gap-8'>
-          <h1 className='text-2xl lg:text-5xl font-bold text-emerald-400'>
-            A Gallery of Experiences:<br />Travel, Food & Collections That Tell a Story
-          </h1>
+          <BoxReveal boxColor={"black"} duration={0.1}>
+            <h1 className='text-2xl lg:text-5xl font-bold text-emerald-400'>
+              A Gallery of Experiences:<br />Travel, Food & Collections That Tell a Story
+            </h1>
+          </BoxReveal>
           <div className='grid grid-cols-12'>
             <div className='col-span-12 lg:col-span-7'>
               <div className="text-sm md:text-base py-2 md:py-8 lg:py-12">
-                <h2>Welcome to a journey through my experiences!<br /><br />This gallery is a glimpse into my travels, the delicious food I&lsquo;ve encountered, and the unique collections I&lsquo;ve built along the way. Each image, story, and treasure tells a tale of exploration, discovery, and the joy of finding beauty in the unexpected. Dive in and explore the world through my lens, where every bite, sight, and find becomes a cherished memory.</h2>
+                <BoxReveal boxColor={"black"} duration={0.25}>
+                  <h2>Welcome to a journey through my experiences!</h2>
+                </BoxReveal>
+                <BoxReveal boxColor={"black"} duration={0.5}>         
+                  <>
+                    <br /><br />
+                    <h2>This gallery is a glimpse into my travels, the delicious food I&lsquo;ve encountered, and the unique collections I&lsquo;ve built along the way. Each image, story, and treasure tells a tale of exploration, discovery, and the joy of finding beauty in the unexpected. Dive in and explore the world through my lens, where every bite, sight, and find becomes a cherished memory.</h2>
+                  </>           
+                </BoxReveal>
               </div>
               <div className='flex flex-col md:flex-row gap-4 pt-4 lg:pt-0'>
-                <Link href='/explore' className="group w-full md:w-1/3">
-                  <ShineBorder
-                    className="text-center uppercase hover:text-emerald-400"
-                    color={["#4ade80", "#a78bfa", "#FFBE7B"]}
-                    borderWidth={2}
-                  >
-                    <div className='flex flex-row justify-between items-center px-2 gap-2'>
-                      <div className='text-lg'>
-                        Explore World Map
+                <BoxReveal boxColor={"black"} duration={0.75}>
+                  <Link href='/explore' className="group w-full md:w-1/3">
+                    <ShineBorder
+                      className="text-center uppercase hover:text-emerald-400"
+                      color={["#4ade80", "#a78bfa", "#FFBE7B"]}
+                      borderWidth={2}
+                    >
+                      <div className='flex flex-row justify-between items-center px-2 gap-2'>
+                        <div className='text-lg'>
+                          Explore World Map
+                        </div>
+                        <div className="mr-2 w-4 h-4 group-hover:translate-x-1"><FaArrowRight /></div>                  
                       </div>
-                      <div className="mr-2 w-4 h-4 group-hover:translate-x-1"><FaArrowRight /></div>                  
-                    </div>
-                  </ShineBorder>
-                </Link>
+                    </ShineBorder>
+                  </Link>
+                </BoxReveal>
               </div>
             </div>
             <div className="hidden xl:block xl:col-span-5 justify-self-center relative">
