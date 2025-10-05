2dbf06255afa5f72b00b99db3611e91a1f46b785
Khai Fahmi Zaki <khaifahmi99@gmail.com>
2025-10-05 14:10:42 +1100

    feat: added japan travel destinations

--- app/_data/travel.ts
+++ app/_data/travel.ts
@@ -30,14 +30,36 @@ export const getTravels = async ({
       return new Date(b.date).getTime() - new Date(a.date).getTime();
     })
     .slice(startIndex, fetchAll ? travels.length : endIndex)
-    .map((item) => ({
-      ...item,
-      id: `${item.folder}`,
-      location: `${item.city}, ${item.country}`,
-      cover: item.cover ? `https://d3ae3kedxtitrj.cloudfront.net/travel/${item.folder}/${item.cover}` : "/placeholder.jpg",
-      capturedOn: item.captured_on,
-      coordinates: [item.latitude, item.longitude],
-    }));
+    .map((item) => {
+
+      // Format like "<2-digit><5-digit-number><.jpeg>...<2-digit><5-digit-numbe><.jpeg>"
+      // eg. "AB00001.jpeg...AB00200.jpeg"
+      const isRange = item.photos.length === 1 && item.photos[0].includes("...");
+      const photos: string[] = isRange ? [] : item.photos;
+      if (isRange) {
+        const [start, end] = item.photos[0].split("...");
+        const prefix = start.slice(0, 2); // "AB"
+        const startNum = parseInt(start.slice(2, 7));
+        const endNum = parseInt(end.slice(2, 7));
+
+        for (let i = startNum; i <= endNum; i++) {
+          const photo = `${prefix}${String(i).padStart(5, "0")}.jpeg`;
+          photos.push(photo);
+        } 
+
+        console.log(`${item.title} is using range photos: ${photos[0]} to ${photos[photos.length - 1]} (${photos.length} photos)`);
+      }
+
+      return ({
+        ...item,
+        id: `${item.folder}`,
+        photos: photos,
+        location: `${item.city}, ${item.country}`,
+        cover: item.cover ? `https://d3ae3kedxtitrj.cloudfront.net/travel/${item.folder}/${item.cover}` : "/placeholder.jpg",
+        capturedOn: item.captured_on,
+        coordinates: [item.latitude, item.longitude],
+      })
+    });
 
   const totalImages = travels.flatMap((travel) => travel.photos).length;
 
